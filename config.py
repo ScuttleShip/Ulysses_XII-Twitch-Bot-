da3159f43639ea96f82b3b1e21c90590ebd5aeb3
ScuttleShip <ScuttleShip@users.noreply.github.com>
2019-06-17 19:24:59 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+HOST = "irc.chat.twitch.tv" # This is Twitchs IRC server
+PORT = 6667 # Twitch IRC server listens on port 6667
+NICK = "BOT NAME" # Twitch username you're using for your bot
+PASS = "OAuth in here" # your Twitch OAuth token
+CHAN = "#USERNAME" # The channel you want your bot to join
+RATE = (20/30.0) # Messages per second
+BAN_PAT = [
+	r"BANWORD",
+]
+COLOUR = [
+	[r"!twitch","Changing the light to Twitch Purple"],
+	[r"!green", "Changing the light to Green"],
+]
+COMMANDS = [
+[r"!invdiscord","Join the TI discord here: https://discord.gg/My9emf8"],
+]
+
